faa4fac13ee2591722b3e0ec185ea9daf981998a
Amir <amirsobhan2004@gmail.com>
2024-04-25 00:04:09 +0200

    Update Bevestiging.java

--- Bevestiging.java
+++ Bevestiging.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
+interface Wijziginginterface {
+    void wijzigMedicijn(MedicijnLijst medicijnLijst);
+}
 
 public class Bevestiging {
     private String message;
     private boolean status;
+
+
+    private Wijziginginterface medicijnWijziger;
+
+    public Bevestiging(Wijziginginterface medicijnWijziger) {
+        this.medicijnWijziger = medicijnWijziger;
+        this.message = "Medicijnen zijn bevestigd";
+        this.status = false;
+    }
+
+    public void wijziging(MedicijnLijst medicijnLijst) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wilt u nog een medicijn wijzigen? (ja/nee)");
+        if (scanner.nextLine().equals("ja")) {
+            medicijnWijziger.wijzigMedicijn(medicijnLijst);
+        }
+    }
 
     public Bevestiging() {
         this.message = "Medicijnen zijn bevestigd";
@@ -13,7 +35,7 @@ public class Bevestiging {
         System.out.println("Het programma is klaar");
     }
 
-    public String getMessage() {
+    public String getMessage() { 
         return message;
     }
 
