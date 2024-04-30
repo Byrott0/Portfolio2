1253db0d95dd4e10cb9a15a8db45699d7676bd00
Amir <amirsobhan2004@gmail.com>
2024-04-30 10:16:34 +0200

    Update Medicijn.java

--- Medicijn.java
+++ Medicijn.java
@@ -5,29 +5,35 @@ import java.util.UUID;
 public class Medicijn {
     private String naam;
     private String innameTijd;
-    private final long innameTijdVerschilMS;
+    private final long innameTijdVerschil;
     private final int id;
 
     public Medicijn(String naam, String innameTijd) {
         this.naam = naam;
         this.innameTijd = innameTijd;
         this.id = UUID.randomUUID().hashCode();
+        //dit genereert een unieke id voor elk medicijn
+        //en hashcode is een unieke waarde voor elk object wat in int teruggegeven wordt
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         LocalTime innameTijdLocal = LocalTime.parse(innameTijd, formatter);
         LocalTime now = LocalTime.now();
+        //datetimeformatter zorgt ervoor dat de tijd in het juiste formaat wordt geprint
+        //localtime.parse zorgt ervoor dat de tijd in het juiste formaat wordt geprint
+        //localtime.now zorgt ervoor dat de tijd van nu wordt geprint
 
-        int innameTijdSeconds = innameTijdLocal.toSecondOfDay();
-        int nowSeconds = now.toSecondOfDay();
-        long tijdVerschil = innameTijdSeconds - nowSeconds;
+        int innameTijdSeconds = innameTijdLocal.toSecondOfDay() / 60;
+        int nowMinutes = now.toSecondOfDay()/ 60;
+        long tijdVerschil = innameTijdSeconds - nowMinutes;
+        //verschil in seconden tussen de inname tijd en de huidige tijd
 
         // Als de tijd negatief is, voeg dan 24 uur (in seconden) toe om de tijd correct in te stellen voor de volgende dag.
         if (tijdVerschil < 0) {
-            tijdVerschil += 24 * 3600;
+            tijdVerschil += 24 * 60;
         }
 
-        this.innameTijdVerschilMS = tijdVerschil * 1000L;
-        System.out.println("Ik ga wachten in seconden: " + innameTijdVerschilMS / 1000L);
+        this.innameTijdVerschil = tijdVerschil * 1000L; //L staat voor long type
+        System.out.println("Ik ga wachten in minuten: " + innameTijdVerschil /1000L);
     }
 
 
@@ -40,7 +46,7 @@ public class Medicijn {
     }
 
     public long getInnameTijdVerschilMS() {
-        return innameTijdVerschilMS;
+        return innameTijdVerschil;
     }
 
     public int getId() {
