241ef9b411113d4df17a07e3e5a85b31730f182b
Amir <amirsobhan2004@gmail.com>
2024-04-30 10:16:49 +0200

    Update MedicijnLijst.java

--- MedicijnLijst.java
+++ MedicijnLijst.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 
-public class MedicijnLijst {
-    private final ArrayList<Medicijn> medicijnen = new ArrayList<>();
+public class MedicijnLijst { // dit staat voor de class medicijn
+
+    private final ArrayList<Medicijn> medicijnen = new ArrayList<>(); //final variabel doet dat je de variabel niet kan veranderen
+    // en in een arraylist zorgt het ervoor dat je niet kan verwijzen naar een andere lijst maar juist specifiek naar deze
 
     public void addMedicijn(Medicijn medicijn) {
         medicijnen.add(medicijn);
@@ -19,7 +21,9 @@ public class MedicijnLijst {
     public ArrayList<Medicijn> getMedicijnen() {
         return medicijnen;
     }
+    //dit is een getter die de medicijnen teruggeeft
 
+//een specifiek Medicijn object te vinden en terug te geven op basis van zijn id.
     public Medicijn getMedicijnById(int id) {
         for (Medicijn medicijn : medicijnen) {
             if (medicijn.getId() == id) {
