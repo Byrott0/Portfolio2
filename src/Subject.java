e97fb7b5562c6dbccd4bd17bfae8149c71a08853
Byrott0 <amirsobhan2004@gmail.com>
2024-06-15 02:39:53 +0200

    begin commit

--- src/Subject.java
+++ src/Subject.java
@@ -4,6 +4,6 @@ public interface Subject {
         void addObserver(Observer observer);
         void removeObserver(Observer observer);
         void notifyObservers();
-        //voeg hierin string of int
+        //voeg hierin string of int test
 
 }
